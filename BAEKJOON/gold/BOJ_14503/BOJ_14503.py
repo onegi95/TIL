c46b951c2dd4a8e4a8938918e6504d8130559690
multicampus <onegison95@gmail.com>
2023-01-30 16:21:37 +0900

    230130| TIL, BOJ GOLD 14503 solved

--- BAEKJOON/gold/BOJ_14503/BOJ_14503.py
+++ BAEKJOON/gold/BOJ_14503/BOJ_14503.py
@@ -4,19 +4,42 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 
-def rotate(x,y,an):
+def rotate(x,y,an,rotate_num):
+    global temp
     if an == 0:
-        temp = [x, y-1,3]
-        return (temp)
+        temp = [x, y-1,3,rotate_num]
     elif an ==1:
-        temp = [x-1, y,0]
-        return (temp)
+        temp = [x-1, y,0,rotate_num]
     elif an ==2:
-        temp = [x, y+1,1]
+        temp = [x, y+1,1,rotate_num]
+    elif an == 3:
+        temp = [x+1, y,2,rotate_num]
+
+    if clean_map[temp[0]][temp[1]] == 0:
         return (temp)
     else:
-        temp = [x+1, y,2]
+        rotate_temp = temp[2]
+        temps = [x , y, rotate_temp, rotate_num+1]
+        return (temps)
+
+def Diririri(x,y,an):
+    global temp,cant_go_back
+    if an == 0:
+        temp = [x + 1, y, an]
+    elif an == 1:
+        temp = [x , y-1, an]
+    elif an == 2:
+        temp = [x-1, y, an]
+    elif an == 3:
+        temp = [x, y+1, an]
+
+    # 만약 후진을 못하면...?
+    if clean_map[temp[0]][temp[1]] == 1:
+        cant_go_back = 1
         return (temp)
+    else:
+        return (temp)
+
 
 for tc in range(T):
     n,m = map(int, input().split())
@@ -26,23 +49,21 @@ for tc in range(T):
         temp = list(map(int, input().split()))
         clean_map.append(temp)
     rotate_num = 0
+    cant_go_back = 0
     result_clean = 0
-    while rotate_num <=4:
+    while cant_go_back == 0:
         if clean_map[x][y] == 0:
             clean_map[x][y] = 5
             result_clean +=1
-            temp = rotate(x,y,A)
-            x1,y1,A1 = temp[0],temp[1],temp[2]
-            #  왼쪽이 빈곳인지 확인 하는 코드 + 아닐경우 로데이션 돌리는 코드
-            while True:
-                if clean_map[x1][y1] == 0:
-                    break
-                else:
-                    rotate_num +=1
-                    temp = rotate(x, y, A1)
-                    x1, y1, A1 = temp[0], temp[1], temp[2]
-                    if rotate_num >= 4:
-                        break
+            rotate_num = 0
+        al_temp = rotate(x,y,A,rotate_num)
+        x,y,A,rotate_num = al_temp[0],al_temp[1],al_temp[2],al_temp[3]
+
+        if rotate_num >=4:
+            #후진을 한다
+            x, y, A =  Diririri(x,y,A)
+            rotate_num =0
+
 
 
     print(result_clean)
