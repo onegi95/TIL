946a09d325a03ab648a380af693a604bd31784d8
multicampus <onegison95@gmail.com>
2023-02-02 17:51:01 +0900

    파이프 옮겨옮겨

--- BAEKJOON/gold/BOJ_17070/17070.py
+++ BAEKJOON/gold/BOJ_17070/17070.py
@@ -14,3 +14,8 @@ for tc in range(T):
         pipe_map.append(temp)
 
     dp_map = [[0 for _ in range(N)]for _ in range(N)]
+
+    #  방향에 대한 불용 죄표 확인하기
+    #  방향이 바뀌명 => 엥글을 따로 바꿔 줘야 함
+    #  즉 가는 방향마다 최소로 갈 수 있는 값을 정해주는 것
+    #  만약 갱신할 수 없는 상황이라면 => 0출력
